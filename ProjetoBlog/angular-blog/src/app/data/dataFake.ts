5d0a25c02b45d9d135d9dd0312faedba6b97328c
raul-carbone <raul.carboneinside@gmail.com>
2024-02-23 02:33:28 -0300

    blog-app-proj

---
+++ ProjetoBlog/angular-blog/src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuTvLpOtAEzvaPdVVoQZROYnSQZxw64MLHWOHaHTFOqgUHyN457qYA940t8AdmONx2QVM&usqp=CAU",
+        "title": "saiu novo filme",
+        "description": "ola"
+    },
+    {
+        "id": "2",
+        "photo":"https://img.freepik.com/free-vector/breaking-news-television-concept_52683-36584.jpg?size=626&ext=jpg&ga=GA1.1.1412446893.1705017600&semt=ais",
+        "title": "saiu novo filme 2",
+        "description": "ola 2"
+    }
+]
